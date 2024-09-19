4a55fb35e99fa8627701bbb00ecbbcd28f0149ba
Doyler <37679344+vemork@users.noreply.github.com>
2024-09-18 21:30:09 -0500

    Delete tourBuilderExample directory

--- tourBuilderExample/src/TourPackageBuilder.java
+++
@@ -1,7 +0,0 @@
-interface TourPackageBuilder {
-  void buildDestination(String destination);
-  void buildAccommodation(String accommodation);
-  void buildTransportation(String transportation);
-  void buildActivities(String activities);
-  TourPackage getTourPackage();
-}
